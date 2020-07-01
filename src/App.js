491365bc46a2bd70a267df1bca65319a46d3aaf2
Victor Iris <victormidp@gmail.com>
2020-07-01 17:30:34 -0500

    Replace ui kit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import { Document, Page, pdfjs, Outline } from "react-pdf";
 import docfile from "./assets/docs/sampledoc.pdf";
+import { Layout, Menu } from 'antd';
+import Header from "./components/Header";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
 	const [pageNumber, setPageNumber] = useState(1);
 	return (
-		<div className="App">
+		<Layout>
+			<Header />
 			<Document file={docfile}>
 				<Outline onItemClick={({ pageNumber }) => setPageNumber(pageNumber)} className="outline" />
 				<Page pageNumber={pageNumber} />
 			</Document>
-		</div>
+		</Layout>
 	);
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const { Header, Content, Footer } = Layout;
+
+function AppHeader() {
+	return (
+		<div className="header">
+
+		</div>
+	);
+}
+
+export default AppHeader;
