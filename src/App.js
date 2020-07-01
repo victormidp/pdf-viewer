6eef54ed54576fbfcd5ac435cc4005598d5cf25e
Victor Iris <victormidp@gmail.com>
2020-07-01 16:41:12 -0500

    Add node-sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import { Document, Page, pdfjs, Outline } from "react-pdf";
 import docfile from "./assets/docs/sampledoc.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -10,7 +9,7 @@ function App() {
 	return (
 		<div className="App">
 			<Document file={docfile}>
-				<Outline onItemClick={({ pageNumber }) => setPageNumber(pageNumber)} />
+				<Outline onItemClick={({ pageNumber }) => setPageNumber(pageNumber)} className="outline" />
 				<Page pageNumber={pageNumber} />
 			</Document>
 		</div>
