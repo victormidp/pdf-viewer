30bb6a2ba8e46351f0fa2d5d3c12da80e2921947
Victor Iris <victormidp@gmail.com>
2020-07-01 19:17:18 -0500

    Implement image controls

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { Button, ButtonGroup } from "reactstrap";
 
-function ImageViewer({item}) {
-    return (
-        <div className="imageViewer">
+function ImageViewer({ item }) {
+	return (
+		<div className="imageViewer">
+			
             <TransformWrapper>
-                <TransformComponent>
-                    <div className="imageViewer__content">
-                        <img src={item.url} alt="test" className="imageViewer__image" />
-                        <div>
-                        {item.title}
+                {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <>
+                        <div class="controlMenu">
+                            <ButtonGroup>
+                                <Button onClick={zoomOut}>-</Button>
+                                <Button onClick={resetTransform}>Reset</Button>
+                                <Button onClick={zoomIn}>+</Button>
+                            </ButtonGroup>
                         </div>
-                    </div>
-                </TransformComponent>
-            </TransformWrapper>
-        </div>
+                        <TransformComponent>
+                            <div className="imageViewer__content">
+                                <img src={item.url} alt="test" className="imageViewer__image" />
+                                <div>{item.title}</div>
+                            </div>
+                        </TransformComponent>
+                    </>
+                )}
+			</TransformWrapper>
+		</div>
 	);
 }
 
